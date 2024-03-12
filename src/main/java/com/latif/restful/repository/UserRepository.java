9292443496c0c78aa158c87867b96a6e726a0f90
latif <latifirfansyah@gmail.com>
2024-03-12 15:18:33 +0700

    add register

---
+++ src/main/java/com/latif/restful/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.latif.restful.repository;
+
+import com.latif.restful.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
---
+++ src/main/java/com/latif/restful/service/UserService.java
@@ -0,0 +1,43 @@
+package com.latif.restful.service;
+
+import com.latif.restful.entity.User;
+import com.latif.restful.model.RegisterUserRequest;
+import com.latif.restful.repository.UserRepository;
+import com.latif.restful.security.BCrypt;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Validator validator;
+
+    public void register(RegisterUserRequest request) {
+        Set<ConstraintViolation<RegisterUserRequest>> constraintViolations = validator.validate(request);
+        if (constraintViolations.size() != 0) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+
+        if (userRepository.existsById((request.getUsername()))) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already registered");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+        user.setName(request.getName());
+
+        userRepository.save(user);
+    }
+}
