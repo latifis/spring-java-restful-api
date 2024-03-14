263d7fafbafa7a8970b1d915fabe34ff3085e0ac
latif <latifirfansyah@gmail.com>
2024-03-14 15:53:11 +0700

    add implementation token

--- src/main/java/com/latif/restful/controller/UserController.java
+++ src/main/java/com/latif/restful/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.latif.restful.controller;
 
 import com.latif.restful.model.RegisterUserRequest;
+import com.latif.restful.entity.User;
+import com.latif.restful.model.UserResponse;
 import com.latif.restful.model.WebResponse;
 import com.latif.restful.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +26,14 @@ public class UserController {
     public WebResponse<String> register(@RequestBody RegisterUserRequest request) {
         userService.register(request);
         return WebResponse.<String>builder().data("OK").build();
+    }
+
+    @GetMapping(
+            path = "/api/users/current",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<UserResponse> get(User user){
+        UserResponse userResponse = userService.get(user);
+        return WebResponse.<UserResponse>builder().data(userResponse).build();
     }
 }
--- src/main/java/com/latif/restful/service/UserService.java
+++ src/main/java/com/latif/restful/service/UserService.java
@@ -2,6 +2,7 @@ package com.latif.restful.service;
 
 import com.latif.restful.entity.User;
 import com.latif.restful.model.RegisterUserRequest;
+import com.latif.restful.model.UserResponse;
 import com.latif.restful.repository.UserRepository;
 import com.latif.restful.security.BCrypt;
 import jakarta.transaction.Transactional;
@@ -38,5 +39,12 @@ public class UserService {
         user.setName(request.getName());
 
         userRepository.save(user);
+    }
+
+    public UserResponse get(User user){
+        return UserResponse.builder()
+                .username(user.getUsername())
+                .name(user.getName())
+                .build();
     }
 }
